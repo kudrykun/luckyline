c5b3f37cda6e17a1e2c043fa632ba75b109643b1
kudrykun <kudrykun@gmail.com>
2016-10-19 23:16:00 +0300

    Orders added

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,14 @@ Rails.application.routes.draw do
 
   resources :news_items, only: [:index, :show]
   resources :opinions, only: [:index, :show, :create]
-  resources :orders, only: [:create]
+  resources :items, only: [] do
+    resources :orders, only: [:create]
+  end
   root 'categories#index'
 
 
   get '/catalog/:category_id', to: 'subcategories#index',as: 'subcategories'
   get '/catalog/:category_id/:subcategory_id', to: 'items#index',as: 'items'
   get '/catalog/:category_id/:subcategory_id/:id', to: 'items#show',as: 'item'
-  get '/order/:item_id/:id', to: 'order#create', as: 'order_item'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
