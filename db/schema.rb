108857886f47ede20f006f8c31fa0851aed3121f
Sergey Vasilenko <kudrykun@gmail.com>
2017-01-06 04:41:47 +0300

    Deleted item_colors, ready for dropbox image storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212160002) do
+ActiveRecord::Schema.define(version: 20170106011649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,17 +51,6 @@ ActiveRecord::Schema.define(version: 20161212160002) do
     t.text     "description"
     t.index ["category_id"], name: "index_galleries_on_category_id", using: :btree
     t.index ["slug"], name: "index_galleries_on_slug", unique: true, using: :btree
-  end
-
-  create_table "item_colors", force: :cascade do |t|
-    t.integer  "item_id"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-    t.index ["item_id"], name: "index_item_colors_on_item_id", using: :btree
   end
 
   create_table "items", force: :cascade do |t|
@@ -132,7 +121,6 @@ ActiveRecord::Schema.define(version: 20161212160002) do
   end
 
   add_foreign_key "galleries", "categories"
-  add_foreign_key "item_colors", "items"
   add_foreign_key "items", "categories"
   add_foreign_key "orders", "items"
 end
