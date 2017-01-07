4dd9ebe18dcb7bb243e4f2ea562b106f3e51bda2
Sergey Vasilenko <kudrykun@gmail.com>
2017-01-07 21:39:29 +0300

    Now images formatted with paperclip in items. But not in modals in items!

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ApplicationRecord
   belongs_to :imageable, polymorphic: true
 
-  has_attached_file :image, styles: { normal: "600x600",medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: {for_show: "800x800",for_grid: "450x450>", thumb: "150x150>", }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
