647d2af8e42adfbd9bbb55141bb9d3d6ff4e9e7b
Sergey Vasilenko <kudrykun@gmail.com>
2017-01-10 03:36:35 +0300

    6 icons, title and description for main

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,8 @@
 class MainController < ApplicationController
   def index
-    set_meta_tags title: 'Lucky-line - мебель для жизни',
-                  description: 'Описание для главной'
+    set_meta_tags title: 'Кухни в Мытищах Luckyline: мебель, фурнитура, бытовая техника',
+                  description: 'Купить кухни в Мытищах с доставкой по Москве и Московской области по приемлемой цене.
+                                  Оценить преимущества магазина LuckyLine и ознакомится с каталогом товаров.'
     @items = Item.all.sample(4)
   end
 end
