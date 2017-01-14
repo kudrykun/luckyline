50441ae53641dbc7345fcb9f26ba72b10e21d206
Sergey Vasilenko <kudrykun@gmail.com>
2017-01-15 00:54:24 +0300

    Nice 4 kitchens in main

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,5 +4,9 @@ class MainController < ApplicationController
                   description: 'Купить кухни в Мытищах с доставкой по Москве и Московской области по приемлемой цене.
                                   Оценить преимущества магазина LuckyLine и ознакомится с каталогом товаров.'
     @items = Item.all.sample(4)
+    @item1 = @items.first
+    @item2 = @items.second
+    @item3 = @items.third
+    @item4 = @items.fourth
   end
 end
