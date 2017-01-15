f2afc1520e3209d46311e9a1f0ba1667c3f39e5b
Sergey Vasilenko <kudrykun@gmail.com>
2017-01-16 02:04:31 +0300

    Alt images, h1 in items#show and new content in items#show

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
   def index
-    set_meta_tags title: 'Кухни LuckyLine: мебель, в Москве,Мытищах и Московской области',
+    set_meta_tags title: 'Кухни LuckyLine – мебель в Москве,Мытищах и Московской области',
                   description: 'Кухни LuckyLine в Мытищах от фабрики – купить кухни в Королёве, Фрязино, Пушкино, Балашихе, Щёлково и других городах Московской области. '
     @items = Item.all.sample(4)
     @item1 = @items.first
--- config/initializers/meta_tags.rb
+++ config/initializers/meta_tags.rb
@@ -1,6 +1,6 @@
 MetaTags.configure do |c|
-  c.title_limit        = 70
-  c.description_limit  = 160
+  c.title_limit        = 100
+  c.description_limit  = 200
   c.keywords_limit     = 255
   c.keywords_separator = ', '
 end
