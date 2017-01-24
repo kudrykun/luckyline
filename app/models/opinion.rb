171db54744a6210f911cc5f9b20a6f92c83691c4
Sergey Vasilenko <kudrykun@gmail.com>
2017-01-24 18:48:09 +0300

    Валидации для отзывов

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,4 +1,12 @@
 class Opinion < ApplicationRecord
   has_attached_file :image, styles: {for_show: "800x800",for_grid: "450x450>", thumb: "150x150>", }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  validates :name, presence: true, format: {with: /\A[а-яА-ЯЁё]{3,15}\Z/,
+                                            message:'Only Cyrillic, in range beetween 3..15'}
+  validates :text, presence: true, length: {in: 10..500}
+
+  validates :vk, format: {with: /\A((https:\/\/vk\.com\/id[\d]+)|(https:\/\/vk\.com\/[a-zA-Z\d_]+)|)\Z/}
+
+  validates :ok, format: {with: /\A((https:\/\/ok\.ru\/profile\/[\d]+)|(https:\/\/ok\.ru\/[a-zA-z\d\.]{5,})|)\Z/}
 end
