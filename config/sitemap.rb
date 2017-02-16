bb6f5a4b6666445fe3357171a0168c1d218638a9
Sergey Vasilenko <kudrykun@gmail.com>
2017-02-16 13:13:59 +0300

    Немного изменил форму и настроил sitemap под новые роуты

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,21 +2,21 @@
 SitemapGenerator::Sitemap.default_host = "http://lucky-line-mebel.ru"
 
 SitemapGenerator::Sitemap.create do
-  add contacts_show_path
+  add contacts_path
   add catalog_path, :priority => 0.7
   add opinions_path, :priority => 0.7
-  add news_items_path, :priority => 0.7
+  add news_items_path, :priority => 0.4
   NewsItem.find_each do |news_item|
-    add news_item_path(news_item), :lastmod => news_item.updated_at, :priority => 0.8
+    add news_item_path(news_item), :lastmod => news_item.updated_at, :priority => 0.4
   end
   Category.find_each do |category|
-    add subs_and_items_path(category), :lastmod => category.updated_at, :priority => 0.6
+    add subs_and_items_path(category), :lastmod => category.updated_at, :priority => 0.8
   end
   Item.find_each do |item|
-    add show_category_item_path(item.category,item), :lastmod =>  item.updated_at, :priority => 0.9
+    add show_item_path(item.category,item), :lastmod =>  item.updated_at, :priority => 0.9
   end
   Gallery.find_each do |gallery|
-    add show_category_gallery_path(gallery.category,gallery), :lastmod => gallery.updated_at, :priority => 0.8
+    add show_gallery_path(gallery.category,gallery), :lastmod => gallery.updated_at, :priority => 0.8
   end
 
 end
