e952dd1d63ec509f0111ed96a25444d6c6842bec
Sergey Vasilenko <kudrykun@gmail.com>
2017-05-08 14:24:56 +0300

    Убрал товары с главной

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,10 +2,12 @@ class MainController < ApplicationController
   def index
     set_meta_tags title: 'Кухни LuckyLine – мебель в Москве,Мытищах и Московской области',
                   description: 'Кухни LuckyLine в Мытищах от фабрики – купить кухни в Королёве, Фрязино, Пушкино, Балашихе, Щёлково и других городах Московской области. '
+=begin
     @items = Item.all.sample(4)
     @item1 = @items.first
     @item2 = @items.second
     @item3 = @items.third
     @item4 = @items.fourth
+=end
   end
 end
